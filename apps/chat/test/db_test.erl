84e6f003d4a73be7660db67920cbe205acb50718
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2018-07-14 19:23:51 -0400

    Some unit tests for data store

---
+++ apps/chat/test/db_test.erl
@@ -0,0 +1,53 @@
+%%%-------------------------------------------------------------------
+%%% @author Albert Cruz
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 0X. Jul 2018 XX:XX
+%%%------------------------------------------------------------------
+-module(db_test).
+-include_lib("eunit/include/eunit.hrl").
+
+-author("Albert Cruz").
+
+double_register_test_() ->
+    {setup,
+     fun start/0, % setup function
+     fun stop/1, % cleanup function
+     fun(SetupData) ->
+             [
+              user_do_not_exists(SetupData)
+             ,register_one_user(SetupData)
+             ,wrong_password(SetupData)
+             ,geting_user(SetupData)
+             ,register_one_user_twice(SetupData)
+             ]
+     end}. % instantiators
+
+start() ->
+    {ok, Pid} = db:start(),
+    Pid.
+
+stop(_) ->
+    db:stop().
+
+user_do_not_exists(_) ->
+    Res = db:get_user("jhon", "pass"),
+    [?_assertEqual({error, user_do_not_exists}, Res)].
+
+register_one_user(_) ->
+    Res = db:add_user("jhon", "pass"),
+    [?_assertEqual(ok, Res)].
+
+wrong_password(_) ->
+    Res = db:get_user("jhon", "pass2"),
+    [?_assertEqual({error, wrong_password}, Res)].
+
+geting_user(_) ->
+    Res = db:get_user("jhon", "pass"),
+    [?_assertEqual(ok, Res)].
+
+register_one_user_twice(_) ->
+    Res = db:add_user("jhon", "pass2"),
+    [?_assertEqual({error, user_exists}, Res)].
